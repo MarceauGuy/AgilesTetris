9343f8166b4850684cbfdce550a4e06002b21b36
Alex <al.collet@orange.fr>
2019-10-16 11:44:33 +0200

    WIP audio
    WIP audio

--- ScriptOptions.js
+++ ScriptOptions.js
@@ -1,6 +1,7 @@
 // JavaScript source code
 
 var audio = new Audio('tetris.mp3');
+var audVol;
 
 $(document).ready(function () {
     $("#audioOption").click(function () {
@@ -23,16 +24,28 @@ $(document).ready(function () {
 function audioOpt() {
     $("#audioOption").hide();
     $("#lvl").hide();
+    $("#return").hide();
+
     $("#tetrisTheme").show();
     $("#ckeckBoxTheme").show();
     $("#highVol").show();
     $("#volume").show();
     $("#lowVol").show();
     $("#returnOpt").show();
-    $("#return").hide();
+
+    if (audVol == null) {
+        audVol = 0.5;
+    }
+
+    document.getElementById("highVol").disabled = true;
+    document.getElementById("lowVol").disabled = true;
+
+    document.getElementById('volume').value = audVol;
 }
 
 function lvlOpt() {
+    $("#returnOpt").show();
+
     $("#audioOption").hide();
     $("#lvl").hide();
     $("#tetrisTheme").hide();
@@ -40,20 +53,38 @@ function lvlOpt() {
     $("#highVol").hide();
     $("#volume").hide();
     $("#lowVol").hide();
-    $("#returnOpt").show();
     $("#return").hide();
 }
 
 function PlayMusic() {
     if (document.getElementById('tetrisTheme').checked) {
+        document.getElementById("highVol").disabled = false;
+        document.getElementById("lowVol").disabled = false;
         audio.play();
+        document.getElementById('ckeckBoxTheme').innerText = "Disable sound";
     }
     else {
+        document.getElementById("highVol").disabled = true;
+        document.getElementById("lowVol").disabled = true;
         audio.pause();
-        audio.currentTime = 0;
+        //audio.currentTime = 0;
+        document.getElementById('ckeckBoxTheme').innerText = "Enable sound";
     }
 }
 
-function AudioVolume() {
-    //ex: audio.volume = 0.2; [ - 0 -> 1 + ]
+function AudioVolume(vol) {
+    if (vol == 1) {
+        if (audVol < 1) {
+            audVol = audVol + 0.1;
+            audio.volume = audVol;
+            document.getElementById('volume').value = audVol;
+        }
+    }
+    else if (vol == 2) {
+        if (audVol > 0) {
+            audVol = audVol - 0.1;
+            audio.volume = audVol;
+            document.getElementById('volume').value = audVol;
+        }
+    }
 }
